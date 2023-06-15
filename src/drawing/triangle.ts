e2935d7c545db3b6f53c1cc391c72f703536520b
Jason Tsorlinis <jason.tsorlinis@jbhifi.com.au>
2023-06-15 12:18:03 +1000

    fix unused import

--- src/drawing/triangle.ts
+++ src/drawing/triangle.ts
@@ -1,5 +1,5 @@
 import { setPixel, viewportTransform } from ".";
-import { Vector2, Vector3 } from "../maths";
+import { Vector3 } from "../maths";
 import { BaseShader } from "../shaders/BaseShader";
 
 export interface Barycentric {
