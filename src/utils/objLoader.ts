cce43cae90b7866954e34e125c7804616e6c4059
Jason Tsorlinis <jason.tsorlinis@jbhifi.com.au>
2023-06-19 01:09:30 +1000

    load uvs from obj file

--- src/utils/objLoader.ts
+++ src/utils/objLoader.ts
@@ -1,13 +1,16 @@
-import { Vector3 } from "../maths";
+import { Vector2, Vector3 } from "../maths";
 
 export const loadObj = (file: string, normalize = false) => {
   const vertices: Vector3[] = [];
   let normals: Vector3[] = [];
+  const uvs: Vector2[] = [];
   const flatNormals: Vector3[] = [];
 
   const tempVerts: Vector3[] = [];
+  const tempUVs: Vector2[] = [];
   const tempNormals: Vector3[] = [];
   const tempTris: number[] = [];
+  const tempUVTris: number[] = [];
   const tempNormalTris: number[] = [];
 
   const lines = file.split("\n");
@@ -16,6 +19,8 @@ export const loadObj = (file: string, normalize = false) => {
     const isQuad = split.length === 5;
     if (split[0] == "v") {
       tempVerts.push(new Vector3(+split[1], +split[2], +split[3]));
+    } else if (split[0] == "vt") {
+      tempUVs.push(new Vector2(+split[1], +split[2]));
     } else if (split[0] == "vn") {
       tempNormals.push(new Vector3(+split[1], +split[2], +split[3]));
     } else if (split[0] == "f") {
@@ -31,6 +36,18 @@ export const loadObj = (file: string, normalize = false) => {
           +split[1].split("/")[0] - 1,
           +split[3].split("/")[0] - 1,
           +split[4].split("/")[0] - 1
+        );
+
+        // Add UVs
+        tempUVTris.push(
+          +split[1].split("/")[1] - 1,
+          +split[2].split("/")[1] - 1,
+          +split[3].split("/")[1] - 1
+        );
+        tempUVTris.push(
+          +split[1].split("/")[1] - 1,
+          +split[3].split("/")[1] - 1,
+          +split[4].split("/")[1] - 1
         );
 
         // Add normals
@@ -52,6 +69,13 @@ export const loadObj = (file: string, normalize = false) => {
           +split[3].split("/")[0] - 1
         );
 
+        // Add UVs
+        tempUVTris.push(
+          +split[1].split("/")[1] - 1,
+          +split[2].split("/")[1] - 1,
+          +split[3].split("/")[1] - 1
+        );
+
         // Add normals
         tempNormalTris.push(
           +split[1].split("/")[2] - 1,
@@ -69,9 +93,13 @@ export const loadObj = (file: string, normalize = false) => {
   for (let i = 0; i < tempTris.length; i++) {
     const vert = tempVerts[tempTris[i]];
     const normal = tempNormals[tempNormalTris[i]];
+    const uv = tempUVs[tempUVTris[i]];
     vertices.push(vert);
     if (normal) {
       normals.push(normal);
+    }
+    if (uv) {
+      uvs.push(uv);
     }
 
     if (vert.x > maxPos.x) maxPos.x = vert.x;
@@ -103,5 +131,5 @@ export const loadObj = (file: string, normalize = false) => {
     normals = flatNormals;
   }
 
-  return { vertices, normals, flatNormals };
+  return { vertices, normals, flatNormals, uvs };
 };
