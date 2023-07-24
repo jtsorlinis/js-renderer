cc047207327460a0a35f1d7d5e39bf15e3b02e60
Jason Tsorlinis <jtsorlinis@gmail.com>
2023-07-24 11:49:33 +0300

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig(({ command }) => {
   if (command === "build") {
     return {
+      build: {
+        target: "esnext",
+      },
       base: "/js-renderer/",
     };
   }
