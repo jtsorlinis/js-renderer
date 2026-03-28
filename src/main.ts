442761d8d7b0f388384e3043ead6c6112650caf5
Jason <jtsorlinis@gmail.com>
2026-03-29 00:47:05 +1100

    .

--- src/main.ts
+++ src/main.ts
@@ -35,11 +35,7 @@ const shadingSlider = document.getElementById(
   "shadingSlider",
 ) as HTMLInputElement;
 const modelDd = document.getElementById("modelDd") as HTMLSelectElement;
-
-const ctx = canvas.getContext("2d");
-if (!ctx) {
-  throw new Error("Could not get canvas context");
-}
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 const getShadingButton = () => {
   return shadingList.querySelector<HTMLButtonElement>(
