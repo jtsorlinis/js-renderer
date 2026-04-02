6183ef6f1f100738d99abfdce3075bd0c841a7ce
Jason <jtsorlinis@gmail.com>
2026-04-03 10:44:29 +1100

    remove khronos

--- src/shaders/pbrHelpers.ts
+++ src/shaders/pbrHelpers.ts
@@ -53,27 +53,6 @@ export const toneMapReinhard = (colour: Vector3, exposure: number) => {
   );
 };
 
-export const toneMapKhronos = (colour: Vector3, exposure: number) => {
-  const exposed = colour.scale(exposure);
-  const x = Math.min(exposed.x, Math.min(exposed.y, exposed.z));
-  const offset = x < 0.08 ? x - 6.25 * x * x : 0.04;
-  const shifted = exposed.subtract(new Vector3(offset, offset, offset));
-  const peak = Math.max(shifted.x, Math.max(shifted.y, shifted.z));
-  const startCompression = 0.76;
-
-  if (peak < startCompression) {
-    return shifted;
-  }
-
-  const d = 1 - startCompression;
-  const newPeak = 1 - (d * d) / (peak + d - startCompression);
-  const compressed = shifted.scale(newPeak / peak);
-  const desaturation = 0.15;
-  const g = 1 - 1 / (desaturation * (peak - newPeak) + 1);
-
-  return mixVec3(compressed, new Vector3(newPeak, newPeak, newPeak), g);
-};
-
 export const fresnelSchlick = (cosTheta: number, f0: Vector3) => {
   const factor = Math.pow(1 - saturate(cosTheta), 5);
   return f0.add(Vector3.One.subtract(f0).scale(factor));
