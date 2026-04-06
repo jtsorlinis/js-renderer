b3a211f6f5e46ec9d165ff7b91ec2838d31d0c88
Jason <jtsorlinis@gmail.com>
2026-04-06 19:02:59 +1000

    Scalarise normal TBN

--- src/shaders/NormalMappedTBN.ts
+++ src/shaders/NormalMappedTBN.ts
@@ -19,7 +19,7 @@ const specStr = 0.5;
 const shininess = 32;
 const ambient = 0.1;
 
-export class TexturedShader extends BaseShader {
+export class NormalMappedTBNShader extends BaseShader {
   // Uniforms are set per draw call from `main.ts`.
   uniforms!: Uniforms;
 
@@ -51,22 +51,34 @@ export class TexturedShader extends BaseShader {
     // Read interpolated values at this pixel.
     const uv = this.interpolateVec2(this.vUV);
     const modelPos = this.interpolateVec3(this.vModelPos);
-    const tangent4 = this.interpolateVec4(this.vTangent);
-    const tangent = tangent4.xyz;
-    const handedNess = tangent4.w < 0 ? -1 : 1;
-
-    const N = this.interpolateVec3(this.vNormal).normalize();
-    const T = tangent.subtractInPlace(N.scale(N.dot(tangent))).normalize();
-    const B = N.cross(T).scaleInPlace(handedNess);
+    const vNormal = this.interpolateVec3(this.vNormal).normalize();
+    const vTangent = this.interpolateVec4(this.vTangent);
+    const handedness = vTangent.w < 0 ? -1 : 1;
 
     // Sample material inputs.
     const colour = this.sample(this.uniforms.texture, uv);
     const normalTS = this.sample(this.uniforms.normalTexture, uv);
 
-    const normal = T.scaleInPlace(normalTS.x)
-      .addInPlace(B.scaleInPlace(normalTS.y))
-      .addInPlace(N.scaleInPlace(normalTS.z))
-      .normalize();
+    // Rebuild TBN in scalar form for performance.
+    const tDotN = vTangent.dot3(vNormal);
+    const Tx = vTangent.x - vNormal.x * tDotN;
+    const Ty = vTangent.y - vNormal.y * tDotN;
+    const Tz = vTangent.z - vNormal.z * tDotN;
+    const TLengthSq = Tx * Tx + Ty * Ty + Tz * Tz;
+    const TScale = TLengthSq > 0.000001 ? 1 / Math.sqrt(TLengthSq) : 0;
+    const T = new Vector3(Tx * TScale, Ty * TScale, Tz * TScale);
+
+    const Bx = (vNormal.y * T.z - vNormal.z * T.y) * handedness;
+    const By = (vNormal.z * T.x - vNormal.x * T.z) * handedness;
+    const Bz = (vNormal.x * T.y - vNormal.y * T.x) * handedness;
+    const B = new Vector3(Bx, By, Bz);
+
+    const Nx = T.x * normalTS.x + B.x * normalTS.y + vNormal.x * normalTS.z;
+    const Ny = T.y * normalTS.x + B.y * normalTS.y + vNormal.y * normalTS.z;
+    const Nz = T.z * normalTS.x + B.z * normalTS.y + vNormal.z * normalTS.z;
+    const NLengthSq = Nx * Nx + Ny * Ny + Nz * Nz;
+    const Nscale = NLengthSq > 0.000001 ? 1 / Math.sqrt(NLengthSq) : 0;
+    const normal = new Vector3(Nx * Nscale, Ny * Nscale, Nz * Nscale);
 
     // Blinn-Phong shading
     const lightDir = this.uniforms.mLightDir;
